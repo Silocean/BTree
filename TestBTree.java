847777d2cd6034f59c186734449182d5b0fcce90
Yunfei Si <silenceocean@live.com>
2016-03-18 20:35:19 +0800

    update

--- TestBTree.java
+++ TestBTree.java
@@ -2,21 +2,21 @@
  * Created by Silocean on 2016-03-18.
  */
 public class TestBTree {
-    public static void main(String[] args) {
-        BTree tree = new BTree();
-        tree.insert(tree, 5, "buzhidao");
-        tree.insert(tree, 8, "xiaonan");
-        tree.insert(tree, 3, "xi");
-        tree.insert(tree, 4, "bai");
-        tree.insert(tree, 7, "amier");
-        tree.insert(tree, 0, "lizhicheng");
-        tree.insert(tree, 1, "chalibai");
-        tree.insert(tree, 9, "nenglishi");
-        String result = tree.search(3);
-        if (result != null) {
-            System.out.println(result);
-        } else {
-            System.out.println("result not found!");
-        }
-    }
+	public static void main(String[] args) {
+		BTree tree = new BTree();
+		tree.insert(5, "buzhidao");
+		tree.insert(8, "xiaonan");
+		tree.insert(3, "xi");
+		tree.insert(4, "bai");
+		tree.insert(7, "amier");
+		tree.insert(0, "lizhicheng");
+		tree.insert(1, "chalibai");
+		tree.insert(9, "nenglishi");
+		String result = tree.search(5);
+		if (result != null) {
+			System.out.println(result);
+		} else {
+			System.out.println("result not found!");
+		}
+	}
 }
